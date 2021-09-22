2832a14ed721ab6f8e99001def873de3ee11da85
Cindy Poon Yeung <cndpoon@gmail.com>
2021-09-22 17:28:48 -0400

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import arrow from "../images/arrow.png";
+import gif from "../images/giphy.gif";
 
 const App = () => {
   return (
@@ -9,9 +11,10 @@ const App = () => {
         <section>
           <h1>Oh hey, React</h1>
         </section>
+        <img src={arrow} alt="arrow" width="150" />
+        <img src={gif} alt="gif" />
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   );
 };
