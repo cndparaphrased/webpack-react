513cbef23251a84be189e4813fe5d2fd3b2b80a6
Cindy Poon Yeung <cndpoon@gmail.com>
2021-10-01 15:07:57 -0400

    comment out

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new MiniCssExtractPlugin(), 
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: "./src/images/favicon.png",
+      //favicon: "./src/images/favicon.png",
     }),
   ],
 
